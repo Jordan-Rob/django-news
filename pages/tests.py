aad267f7c13d6bf3dd22626874b88ded1ad6145a
jordanrob709 <jordanrob709@gmail.com>
2020-02-20 18:06:11 +0300

    added tests

--- pages/tests.py
+++ pages/tests.py
@@ -18,7 +18,7 @@ class HomePageTest(SimpleTestCase):
     def test_home_page_uses_correct_template(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'home.htl')
+        self.assertTemplateUsed(response, 'home.html')
 
 
 class SignUpPageTest(TestCase):
@@ -40,7 +40,10 @@ class SignUpPageTest(TestCase):
         self.assertTemplateUsed(response, 'signup.html')
 
     def test_signup_form(self):
-        new_user = get_user_model().objects.create(
+        new_user = get_user_model().objects.create_user(
             self.username, self.email
         )
-        self.assertEqual()
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, 'newuser')
+        self.assertEqual(get_user_model().objects.all()
+                         [0].email, 'newuser@email.com')
